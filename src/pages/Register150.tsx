e157c77455fa39b5149e1395fb771e25b6b51230
hancksoft-afk <hancksoft@gmail.com>
2025-09-06 23:02:12 -0500

    Update Register150.tsx

--- src/pages/Register150.tsx
+++ src/pages/Register150.tsx
@@ -101,8 +101,8 @@ const Register150 = () => {
         {/* Header */}
         <header className="relative mx-auto max-w-6xl px-4 pt-6 flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <div className="h-[67px] w-[78px] grid place-items-center rounded-md ring-white/10 overflow-hidden">
-              <img src="/lovable-uploads/cd470647-4cd4-41f7-aac8-b64cff8b85ba.png" alt="CicloVida Logo" className="h-full w-full object-cover rounded-md" />
+            <div className="h-[67px] w-[78px] grid place-items-center  ring-white/10 overflow-hidden">
+              <img src="/lovable-uploads/cd470647-4cd4-41f7-aac8-b64cff8b85ba.png" alt="CicloVida Logo" className="h-full w-full object-cover " />
             </div>
             <div className="h-[52px] w-[1px] bg-white/10" />
             <p className="text-sm text-muted-foreground">Bienvenido</p>
